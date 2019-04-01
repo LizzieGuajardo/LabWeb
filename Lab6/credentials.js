1a3c019dc8df77dae5b675c16c36e7343ef77dcd
LizzieGuajardo <32180546+LizzieGuajardo@users.noreply.github.com>
2019-03-31 19:03:53 -0600

    lab 6.1

--- Lab6/credentials.js
+++ Lab6/credentials.js
@@ -1,7 +1,7 @@
 //Lizzie M. Guajardo Mozo
 //A00818258
-//Lab 5
+//Lab 6
 
-module.exports.MAPBOX_TOKEN = 'pk.eyJ1IjoibGl6emllZ3VhamFyZG8iLCJhIjoiY2p0M3A0dzdwMmtmMDQzb2Rmc3J6YzhsbiJ9.HSk1dyivS77e9eqYhJOCtw'
+module.exports.MAPBOX_TOKEN = ''
 
-module.exports.DARK_SKY_SECRET_KEY = '57a606a3ad5f53829792e02f87b70a61'
+module.exports.DARK_SKY_SECRET_KEY = ''
